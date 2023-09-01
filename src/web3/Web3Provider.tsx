0bc8a1392a3de06a5a6d026b94df2dc4aa10dd21
vanduc2514 <vanduc2514@gmail.com>
2023-09-02 00:26:15 +0700

    clean up code

--- src/web3/Web3Provider.tsx
+++ src/web3/Web3Provider.tsx
@@ -1,21 +1,18 @@
-import { Web3ReactProvider } from "@web3-react/core";
-import { ethers } from "ethers";
-// import { Web3Provider } from "@ethersproject/providers";
+import { Web3ReactProvider } from "@web3-react/core"
+import { ethers } from "ethers"
 
-// function getLibrary(provider: any) {
-//   return new Web3Provider(provider);
-// }
 const getLibrary = (provider: any) => {
-  const library = new ethers.providers.Web3Provider(provider);
-  library.pollingInterval = 8000; // frequency provider is polling
-  return library;
-};
+  const library = new ethers.providers.Web3Provider(provider)
+  library.pollingInterval = 8000 // frequency provider is polling
+  return library
+}
 
 const WebProvider = ({ children }: any) => {
   return (
-    <Web3ReactProvider getLibrary={getLibrary}>{children}</Web3ReactProvider>
-    // <div>{children}</div>
-  );
-};
+    <Web3ReactProvider getLibrary={getLibrary}>
+      {children}
+    </Web3ReactProvider>
+  )
+}
 
-export default WebProvider;
+export default WebProvider
